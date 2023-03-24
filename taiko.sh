1c9d187b6d271b85db3998661592c2c0185f4189
ntwrkr <83679066+ntwrkrcrt@users.noreply.github.com>
2023-03-25 01:26:19 +0300

    Update taiko.sh

--- taiko.sh
+++ taiko.sh
@@ -11,20 +11,9 @@ echo -e "${fmt}\nSetting up dependencies / Устанавливаем необх
                 cd $HOME
                 sudo apt update
                 sudo apt install curl ca-certificates curl gnupg lsb-release jq -y < "/dev/null"
-if ! docker --version; then
-                . /etc/*-release
-                rm /usr/share/keyrings/docker-archive-keyring.gpg
-                sudo wget -qO- "https://download.docker.com/linux/ubuntu/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-                echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                sudo apt update
-                sudo apt install docker-ce docker-ce-cli containerd.io -y
-fi
-
-if ! docker-compose --version; then
-                docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
-                sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
-                sudo chmod +x /usr/bin/docker-compose
-                . $HOME/.bash_profile
+                
+if ! command -v docker &> /dev/null && ! command -v docker-compose &> /dev/null; then
+                sudo wget https://raw.githubusercontent.com/fackNode/requirements/main/docker.sh && chmod +x docker.sh && ./docker.sh
 fi
 
 echo -e "${fmt}\nInstalling node files / Устанавливаем файлы ноды${end}" && sleep 1
